3b3c171ce43f9d8dc262438e5312547ec6f3aa95
hanabi1224 <harlowmoo@gmail.com>
2020-03-26 19:28:32 +0800

    Upgrade zxing-android-embedded

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hanabi1224/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hanabi1224/git/hanabi1224/flutter_qr_reader/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hanabi1224/sdk/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
